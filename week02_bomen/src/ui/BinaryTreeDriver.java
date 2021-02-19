74500bc29058564b6532e8dd5c1e4d99d96345ab
Devy Leskens <devy.leskens@hotmail.be>
2021-02-19 12:04:36 +0100

    All exercises done

--- week02_bomen/src/ui/BinaryTreeDriver.java
+++ week02_bomen/src/ui/BinaryTreeDriver.java
@@ -3,23 +3,41 @@ package ui;
 import domain.BinaryTree;
 
 public class BinaryTreeDriver {
+    public static void main(String[] args) {
+        //bladeren
+        BinaryTree<String> nodeA = new BinaryTree<>("A");
+        BinaryTree<String> nodeC = new BinaryTree<>("C");
+        BinaryTree<String> nodeE = new BinaryTree<>("E");
+        BinaryTree<String> nodeH = new BinaryTree<>("H");
+        BinaryTree<String> nodeD = new BinaryTree<>("D", nodeC, nodeE);
+        BinaryTree<String> nodeB = new BinaryTree<>("B", nodeA, nodeD);
+        BinaryTree<String> nodeI = new BinaryTree<>("I", nodeH, null);
+        BinaryTree<String> nodeG = new BinaryTree<>("G", null, nodeI);
+        // boom heeft root C en heeft links A en rechts G
+        BinaryTree<String> boom = new BinaryTree<>("F", nodeB, nodeG);
+        boom.printPreorder();
+        System.out.println("\n");
+        boom.printInorder();
+        System.out.println("\n");
+        boom.printPostorder();
 
-	public static void main(String[] args) {
-		BinaryTree<String> nodeD = new BinaryTree<>("D");
-		BinaryTree<String> nodeF = new BinaryTree<>("F");
-		BinaryTree<String> nodeB = new BinaryTree<>("B");
-		BinaryTree<String> nodeH = new BinaryTree<>("H");
-		
-		// knoop A heeft links D en rechts F
-		BinaryTree<String> nodeA = new BinaryTree<>("A",nodeD, nodeF);
-		// knoop E heeft links H
-		BinaryTree<String> nodeE = new BinaryTree<>("E",nodeH,null);
-		// knoop G heeft links E en rechts B
-		BinaryTree<String> nodeG = new BinaryTree<>("G",nodeE, nodeB);
-		
-		// boom heeft root C en heeft links A en rechts G
-		BinaryTree<String> boom = new BinaryTree<>("C",nodeA, nodeG);
-		boom.printPreorder();
-	}
+        System.out.println("\n");
+        System.out.println(boom.countNodes());
 
+        System.out.println("\n");
+        System.out.println(boom.getDepth());
+
+        System.out.println("\n");
+        System.out.println(nodeA.isLeaf());
+
+        System.out.println("\n");
+        System.out.println(boom.getDepth());
+
+        System.out.println("\n");
+        System.out.println(boom.getDataLeaves());
+
+        System.out.println("\n");
+        System.out.println(boom.contains("E"));
+
+    }
 }
